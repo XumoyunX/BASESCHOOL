e60b85c0926217b69198b4f9c5282d2b0a0385db
XumoyunX <xumoyuntoxtayev19@gmail.com>
2024-09-12 10:44:47 +0500

    salom

--- baseschool/settings.py
+++ baseschool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4o#@l061m94m0say=%6jl#r9cot4+*t17qe(g8ic@o+n@f=*7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
